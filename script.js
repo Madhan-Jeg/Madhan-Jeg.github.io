76927fbd036d5a0267310473d1ca4efe43fd6668
Madhan Jeganathan <97021146+Madhan-Jeg@users.noreply.github.com>
2024-01-16 03:40:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelectorAll('[data-tab-title]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTitle)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        target.classList.add('active')
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+    })
+})
+
